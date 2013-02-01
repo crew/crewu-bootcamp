5ceb41be48e31984a82e09b4c2a053bac79529e9
Joshua Caron <jcaron@ccs.neu.edu>
2013-01-31 23:53:10 -0500

    Updated with basic website layout

---
+++ site/includes/header.php
@@ -0,0 +1,35 @@
+<div class="header">
+	<img class="cover" src="">
+</div>
+
+<div id="left_col">
+	<ul>
+		<li>Link</li>
+		<li>Link</li>
+		<li>Link</li>
+		<li>Link</li>
+		<li>Link</li>
+		<ul>
+			<li>Link</li>
+			<li>Link</li>
+			<li>Link</li>
+			<ul>
+				<li>Link</li>
+				<li>Link</li>
+				<li>Link</li>
+				<li>Link</li>
+				<li>Link</li>
+				<li>Link</li>
+			</ul>
+		</ul>
+		<li>Link</li>
+		<li>Link</li>
+		<li>Link</li>
+		<li>Link</li>
+		<li>Link</li>
+		<li>Link</li>
+		<li>Link</li>
+		<li>Link</li>
+
+	</ul>
+</div>
